a92efff83f5f23f47183c1dea8336486782322d3
Abraxas3d <w5nyv@yahoo.com>
2024-04-23 01:46:29 +0000

    Update axi_opv4upr_ip.tcl
    version used in firmware tests

--- hdl/library/axi_opv4upr/axi_opv4upr_ip.tcl
+++ hdl/library/axi_opv4upr/axi_opv4upr_ip.tcl
@@ -26,7 +26,6 @@ source $ad_hdl_dir/library/scripts/adi_ip_xilinx.tcl
 
 adi_ip_create axi_opv4upr
 
-
 adi_ip_files axi_opv4upr [list \
 	"axi_opv4upr.vhd"]
 
@@ -37,41 +36,4 @@ adi_ip_files axi_opv4upr [list \
 adi_ip_properties_lite axi_opv4upr
 
 
-adi_add_bus "s_axis" "slave" \
-        "xilinx.com:interface:axis_rtl:1.0" \
-        "xilinx.com:interface:axis:1.0" \
-        [list \
-		{"s_axis_ready" "TREADY"} \
-          	{"s_axis_valid" "TVALID"} \
-          	{"s_axis_data" "TDATA"} \
-          	{"s_axis_strb" "TSTRB"} \
-          	{"s_axis_keep" "TKEEP"} \
-          	{"s_axis_user" "TUSER"} \
-          	{"s_axis_id" "TID"} \
-          	{"s_axis_dest" "TDEST"} \
-          	{"s_axis_last" "TLAST"}\
-	]
-
-adi_add_bus_clock "s_axis_aclk" "s_axis"
-
-
-
-adi_add_bus "m_axis" "master" \
-	"xilinx.com:interface:axis_rtl:1.0" \
-  	"xilinx.com:interface:axis:1.0" \
-  	[list \
-		{"m_axis_ready" "TREADY"} \
-    		{"m_axis_valid" "TVALID"} \
-    		{"m_axis_data" "TDATA"} \
-  	]
-
-
-set_property name "axi_opv4upr" [ipx::current_core]
-set_property display_name "IQ Modification Station" [ipx::current_core]
-set_property description "IQ Modification Station" [ipx::current_core]
-
-
-#set cc [ipx::current_core]
-
 ipx::save_core [ipx::current_core]
-
